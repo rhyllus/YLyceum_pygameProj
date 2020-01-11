a191f47057324dc8a0d5bc0127b8d95f7dae20b5
- <sashatcher2013@yandex.ru>
2020-01-11 18:15:34 +0300

    main script updated

--- player.py
+++ player.py
@@ -16,50 +16,62 @@ class Player(Sprite):
         self.rect = Rect(x, y, 40, 40)
         self.on_ground = False
 
-    def update(self, left, right):
+    def update(self, left, right, run):
         if left:
-            self.vel_x -= 0.2
+            self.vel_x -= 0.4
         if right:
-            self.vel_x += 0.2
+            self.vel_x += 0.4
         if not left and not right:
-            if self.vel_x <= 0.2:
-                for i in range(2):
+            if self.vel_x <= 0.4:
+                for i in range(4):
                     if self.vel_x <= 0:
-                        self.vel_x += 0.2
-            elif self.vel_x >= 0.2:
-                for i in range(2):
+                        self.vel_x += 0.4
+            elif self.vel_x >= 0.4:
+                for i in range(4):
                     if self.vel_x >= 0:
-                        self.vel_x -= 0.2
+                        self.vel_x -= 0.4
             else:
                 self.vel_x = 0
+        if self.vel_x > 7 and not run:
+            self.vel_x = 7
+        elif self.vel_x < -7 and not run:
+            self.vel_x = -7
+        elif self.vel_x > 10 and run:
+            self.vel_x = 10
+        elif self.vel_x < -10 and run:
+            self.vel_x = -10
         if not self.on_ground:
-            self.vel_y += 0.35
+            self.vel_y += 0.5
         else:
             self.vel_y = 0
 
-    def move(self, platforms):
+    def move(self, platforms, score):
         self.rect.y += self.vel_y
-        self.collision(0, self.vel_y, platforms)
+        platforms, score = self.collision(0, self.vel_y, platforms, score)
         self.rect.x += self.vel_x
-        self.collision(self.vel_x, 0, platforms)
+        platforms, score = self.collision(self.vel_x, 0, platforms, score)
+        return platforms, score
 
-    def collision(self, vel_x, vel_y, platforms):
+    def collision(self, vel_x, vel_y, platforms, score):
         for p in platforms:
-            if sprite.collide_rect(self, p):
+            if sprite.collide_rect(self, p[0]):
                 if vel_x > 0:
-                    self.rect.right = p.rect.left
+                    self.rect.right = p[0].rect.left
                     self.vel_x = 0
                 if vel_x < 0:
-                    self.rect.left = p.rect.right
+                    self.rect.left = p[0].rect.right
                     self.vel_x = 0
                 if vel_y > 0:
-                    self.rect.bottom = p.rect.top
+                    self.rect.bottom = p[0].rect.top
                     self.on_ground = True
                     self.vel_y = 0
                 if vel_y < 0:
-                    self.rect.top = p.rect.bottom
+                    self.rect.top = p[0].rect.bottom
                     self.vel_y = 0
+                if p[1] == 's' and vel_y < 0:
+                    if p[2] == 1:
+                        score += 100
+                        platforms[platforms.index(p)] = (p[0], p[1], 0)
             else:
                 self.on_ground = False
-
-
+        return platforms, score
--- project_att1.py
+++ project_att1.py
@@ -6,6 +6,7 @@ size_lv = (2800, 2800)
 window = pygame.display.set_mode(size_w)
 screen = pygame.Surface(size_lv)
 running = True
+pygame.init()
 
 
 class Platform(pygame.sprite.Sprite):
@@ -19,6 +20,17 @@ class Platform(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, 40, 40)
 
 
+class CoinBox(pygame.sprite.Sprite):
+    def __init__(self, x, y, tile=None):
+        pygame.sprite.Sprite.__init__(self)
+        if tile is not None:
+            self.image = pygame.image.load(tile)
+        else:
+            self.image = pygame.Surface((40, 40))
+            self.image.fill((210, 60, 60))
+        self.rect = pygame.Rect(x, y, 40, 40)
+
+
 def build_stage(level):
     x = 0
     y = 0
@@ -26,14 +38,18 @@ def build_stage(level):
         for j in i:
             if j == '1':
                 pl = Platform(x, y)
-                platforms.append(pl)
+                objects.append((pl, '1'))
+                entities.add(pl)
+            if j == 's':
+                pl = CoinBox(x, y)
+                objects.append((pl, 's', 1))
                 entities.add(pl)
             x += 40
         y += 40
         x = 0
 
 
-platforms = []
+objects = []
 level = ['1111111111111111111111111111111111111111',
          '1111111111111111111111111111111111111111',
          '111111..........................11111111',
@@ -43,7 +59,7 @@ level = ['1111111111111111111111111111111111111111',
          '111111...111111111..............11111111',
          '111111..........................11111111',
          '111111..........................11111111',
-         '111111..11............111111111.11111111',
+         '111111..11ssssssssssss111111111.11111111',
          '111111..........................11111111',
          '111111..........................11111111',
          '1111111111111111111111111111111111111111',
@@ -56,8 +72,10 @@ level = ['1111111111111111111111111111111111111111',
 player = Player(440, 440)
 entities = pygame.sprite.Group()
 entities.add(player)
+score = 0
+font = pygame.font.SysFont(None, 50)
 build_stage(level)
-left = right = jump = False
+left = right = jump = run = False
 clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
@@ -68,23 +86,30 @@ while running:
                 left = True
             if event.key == pygame.K_RIGHT:
                 right = True
+            if event.key == 304:
+                run = True
             if event.key == pygame.K_UP:
-                a = abs(player.vel_y)
-                if abs(player.vel_y) in [0, 0.35, 0.7]:
-                    player.vel_y -= 10
+                if abs(player.vel_y) in [0, 0.5, 1]:
+                    player.vel_y -= 12
                     player.on_ground = False
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 left = False
             if event.key == pygame.K_RIGHT:
                 right = False
+            if event.key == 304:
+                run = False
     screen.fill((100, 255, 100))
-    player.update(left, right)
-    player.move(platforms)
-    clock.tick(60)
+    player.update(left, right, run)
+    objects, score = player.move(objects, score)
     entities.draw(screen)
+    text_str = 'Score: {}'.format(score)
+    text = font.render(text_str, 1, (0, 0, 0))
+    text_x = player.rect.x + 150 - len(str(score)) * 10
+    text_y = player.rect.y - 230
+    screen.blit(text, (text_x, text_y))
     window.blit(screen, (-player.rect.x + 320, -player.rect.y + 240))
+    clock.tick(60)
     pygame.display.flip()
 
-pygame.init()
 pygame.quit()
