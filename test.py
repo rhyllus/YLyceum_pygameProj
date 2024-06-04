71bf897287bc4af1035d922ea4816ea6ceb1b379
rhyllus <sashatcher2013@yandex.ru>
2024-06-04 19:25:14 +0300

    archive from 2020

---
+++ test.py
@@ -0,0 +1,23 @@
+import pygame
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+size = width, height = 800, 600
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+def draw():
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, Pygame!", 1, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    screen.blit(text, (text_x, text_y))
+
+# смена (отрисовка) кадра:
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    draw()
+    pygame.display.flip()
+# завершение работы:
+pygame.quit()
